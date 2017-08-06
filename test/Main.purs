71ef70d22434c040af09b2b84547d17a29df48a2
Beau Lyddon <lyddonb@gmail.com>
2017-08-05 19:26:42 -0600

    Get working with latest Purescript & Google Cloud Node SDK
    Most changes due to the upgrade from 0.10 to 0.11. A couple other
    formatting, documentation tweaks.

--- test/Main.purs
+++ test/Main.purs
@@ -3,15 +3,16 @@ module Test.Main where
 import Prelude                     (Unit, pure, show, bind, (<>),
                                     ($), class Show, unit)
 
-import Control.Monad.Aff           (launchAff)
+import Control.Monad.Aff           (Aff, launchAff)
 import Control.Monad.Aff.Console   (logShow, log)
 import Control.Monad.Eff           (Eff)
 import Control.Monad.Eff.Console   (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Control.Monad.State.Trans   (evalStateT)
+import Control.Monad.State.Trans   (StateT, evalStateT)
 import Control.Monad.Trans.Class   (lift)
-import Data.Argonaut.Decode        (class DecodeJson, gDecodeJson)
-import Data.Argonaut.Encode        (class EncodeJson, gEncodeJson)
+import Data.Argonaut.Decode        (class DecodeJson)
+import Data.Argonaut.Encode        (class EncodeJson)
+import Data.Argonaut.Generic.Argonaut as GA
 import Data.Generic                (class Generic)
 import Data.Lens                   (lens)
 import Data.Maybe                  (Maybe(..))
@@ -31,13 +32,13 @@ testData = HereIsData { bar: "hello", baz: 123, quux: true }
 testData2 :: HereIsData
 testData2 = HereIsData { bar: "hello_number 2", baz: 456, quux: false }
 
-derive instance genericHereIsData :: Generic HereIsData
+derive instance genericHereIsData :: Generic HereIsData 
 
 instance encodeHereIsData :: EncodeJson HereIsData where
-  encodeJson = gEncodeJson
+  encodeJson = GA.encodeJson
 
 instance decodeHereIsData :: DecodeJson HereIsData where
-  decodeJson = gDecodeJson
+  decodeJson = GA.decodeJson
 
 instance showHereIsData :: Show HereIsData where
   show (HereIsData x) = "HereIsData: " <> x.bar <> " " <> show x.baz <> " " <> show x.quux
@@ -48,23 +49,28 @@ data Config
 instance configHasDatastore :: HasDatastore Config where
   _datastore = lens (\(Config x) -> (x.datastore)) (\d x -> Config { datastore: x })
 
-main :: Eff (console :: CONSOLE, datastore :: DATASTORE, err :: EXCEPTION) Unit
+saveData 
+  :: forall s eff. HasDatastore s => 
+  StateT s (Aff (datastore :: DATASTORE | eff)) (Array HereIsData)
+saveData = do
+  key <- keyWithId (Kind "FOO") (Id 1)
+  key2 <- keyWithId (Kind "FOO") (Id 2)
+  _ <- save key Upsert testData2
+  _ <- save key2 Upsert testData
+  getres :: Array HereIsData <- get (key :| []) Nothing
+  q <- createQuery Nothing (Kind "FOO") [
+    limit 2
+  ]
+  qres :: Array HereIsData <- lift (queryRunUntilComplete q (Just Eventual))
+  pure qres
+
+main :: forall eff. Eff (datastore :: DATASTORE, console :: CONSOLE, exception :: EXCEPTION | eff) Unit
 main = do
-  ds <- configuredDatastore (ProjectId "hello") (Credentials "")
-  launchAff $ do
-    res <- evalStateT prog (Config { datastore: ds })
-    log "Finished evaluating program"
+  ds <- configuredDatastore (ProjectId "test") (Credentials "")
+  _ <- launchAff $ do
+    res <- evalStateT saveData (Config { datastore: ds })
+    _ <- log "Finished evaluating program"
     logShow res
   pure unit
-  where prog = do
-          key <- keyWithId (Kind "FOO") (Id 1)
-          key2 <- keyWithId (Kind "FOO") (Id 2)
-          save key Upsert testData2
-          save key2 Upsert testData
-          getres :: Array HereIsData <- get (key :| []) Nothing
-          q <- createQuery Nothing (Kind "FOO") [
-            limit 2
-          ]
-          qres :: Array HereIsData <- lift (queryRunUntilComplete q (Just Eventual))
-          pure qres
+
 
